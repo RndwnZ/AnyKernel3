869f530f5a0e910b692a60d3981b9737e5a0623b
Zan <122100768+RndwnZ@users.noreply.github.com>
2023-07-22 14:12:18 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Evolved Kernel For Viva By RnwdnZan
+kernel.string=Kernel For Redmi Note 11 Pro By RnwdnZan
 do.devicecheck=1
 do.modules=0
 do.systemless=1
